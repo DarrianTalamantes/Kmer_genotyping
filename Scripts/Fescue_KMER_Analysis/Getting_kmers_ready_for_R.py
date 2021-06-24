d75fc40f85d8be604e116bcb558615590406010f
DarrianTalamantes <darrianrtalamantes6@gmail.com>
2021-06-24 16:56:55 -0400

    Made a program to get the kmers ready for R analysis

--- Scripts/Fescue_KMER_Analysis/Getting_kmers_ready_for_R.py
+++ Scripts/Fescue_KMER_Analysis/Getting_kmers_ready_for_R.py
@@ -8,23 +8,29 @@ from os import walk
 
 def main():
     args = parse_args()
-    progeny_kmers = importfile(args.filtered_progeny_file)
+    kmer_file = importfile(args.kmer_file)
 
+    usefull_kmer_list = importfile(args.kmer_list)
 
-    # Next step is use the progeny file to filter the parent file
-    d = {}
-    two_col_file = open(args.filtered_parent_file)
-    for line in two_col_file:
-        key, value = line.split()
-        d[key] = value
-        # The code below keeps the values for d while filtering the keys for d using progeny_kmers
-        parents_filtered_more = dict((key, d[key]) for key in [k for k in progeny_kmers.keys() if k in d])
-    print(len(progeny_kmers))
-    print(len(parents_filtered_more))
+    filtered_kmers = dict()
+    # creates a set of the keys that are found in both these dictionaries
+    kmer_file.intersection(usefull_kmer_list)
+    for key in usefull_kmer_list:
+        if key in kmer_file.keys():
+            d = {key : kmer_file[key]}
+            filtered_kmers.update(d)
+        else:
+            d = {key : kmer_file[key]}
+            filtered_kmers.update(d)
+
+    # The code below keeps the values for d while filtering the keys for d using progeny_kmers
+
+    print(len(kmer_file))
+    print(len(filtered_kmers))
     # print(parents_filtered_more.values())
 
     outfile = open(args.save_file, "w")
-    for key, value in parents_filtered_more.items():
+    for key, value in filtered_kmers.items():
         outfile.write(key + "\t" + str(value))
         outfile.write("\n")
     outfile.close()
@@ -39,10 +45,11 @@ def importfile(file):
         d[key] = value
     return d
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--parent-file", help="File with kmers that will be filtered and saved")
-    parser.add_argument("-c", "--filtered-progeny-file", help="File with kmers that will be used to filter")
+    parser.add_argument("-i", "--kmer-file", help="File with kmers that will be filtered and saved")
+    parser.add_argument("-l", "--kmer-list", help="Kmers to be used to filter the other file with")
     parser.add_argument("-s", "--save-file", help="path and file name to save the filtered kmer file")
     parser.add_argument("--debug", default=False, action="store_true")
     args = parser.parse_args()
--- Scripts/Kmer_Filter_Sapelo2.sh
+++ Scripts/Kmer_Filter_Sapelo2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #HEADER FOR SUBMITTED SCRIPTS
-#SBATCH --job-name=Filtering
-#SBATCH --partition=highmem_p 
+#SBATCH --job-name=Ready_for_R
+#SBATCH --partition=batch 
 #SBATCH --ntasks=1
 #SBATCH  --nodes=1 
 #SBATCH  --cpus-per-task=10
-#SBATCH --time=168:00:00
+#SBATCH --time=24:00:00
 #SBATCH --export=NONE
-#SBATCH --mem=850gb
+#SBATCH --mem=100gb
 #SBATCH --mail-user=drt83172@uga.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=/scratch/drt83172/Wallace_lab/TallFescue/Scripts/OutFiles/%x_%j.out 
@@ -22,6 +22,9 @@
 # echo
 
 # cd $PBS_O_WORKDIR #to use the directory from which the job is submitted as the working directory (where to find input files or binaries)
+
+#Peak memory consumption of thsi program was 756.11 GB
+# #First three steps took 9 hours to complete.
 
 #loading modules
 module load Miniconda2/4.7.10
@@ -44,11 +47,18 @@ InterFiles=$Home/InterFiles
 Parent_KMERS=$Home/KMC_Parent_Data/KMERS
 Final_Kmers=$Home/Kmers_Final
 Parent_stage_2=$Home/Parent_stage_2
+Parents_R_ready=$Home/Parents_R_ready
+Progeny_R_ready=$Home/Progeny_R_ready
+
+
 if [ ! -e $Progeny_KMERS ] ; then mkdir $Progeny_KMERS; fi
 if [ ! -e $InterFiles ] ; then mkdir $InterFiles; fi
 if [ ! -e $Parent_KMERS ] ; then mkdir $Parent_KMERS; fi
 if [ ! -e $Final_Kmers ] ; then mkdir $Final_Kmers; fi
 if [ ! -e $Parent_stage_2 ] ; then mkdir $Parent_stage_2; fi
+if [ ! -e $Parents_R_ready ] ; then mkdir $Parents_R_ready; fi
+if [ ! -e $Progeny_R_ready ] ; then mkdir $Progeny_R_ready; fi
+
 
 ### Below is for practice 
 # Home=/scratch/drt83172/Wallace_lab/TallFescue/Data/Practice
@@ -66,19 +76,31 @@ if [ ! -e $Parent_stage_2 ] ; then mkdir $Parent_stage_2; fi
 # Run program
 
 # Filters for progeny kmers that appear at least x times
-python upper_lower_filter.py -k $Progeny_KMERS -l 6 -s $InterFiles/kmer_progeny_filter1.txt
-# Need to filter one parent file at a time using script 2
->$InterFiles/DeleteMe.txt
-for i in $(ls $Parent_KMERS | cut -d . -f 1)
+# python upper_lower_filter.py -k $Progeny_KMERS -l 6 -s $InterFiles/kmer_progeny_filter1.txt
+# # Need to filter one parent file at a time using script 2
+# >$InterFiles/DeleteMe.txt
+# for i in $(ls $Parent_KMERS | cut -d . -f 1)
+# do
+# echo "python filter_one_dic_using_another.py -p $Parent_KMERS/${i}.txt -c $InterFiles/kmer_progeny_filter1.txt -s $Parent_stage_2/${i}_Stage2.txt" >> $InterFiles/DeleteMe.txt
+# done
+# cat $InterFiles/DeleteMe.txt | parallel --jobs 3 --progress
+# # use script one on second stage parent files
+# python upper_lower_filter.py -k $Parent_stage_2 -u 1 -s $Final_Kmers/usefull_kmers.txt
+#  
+# Use the usefull_kmers list to extract usefull kmers from all other files
+>$InterFiles/EraseMe.txt
+for file in $(ls $Parent_stage_2 | cut -d . -f 1)
 do
-echo "python filter_one_dic_using_another.py -p $Parent_KMERS/${i}.txt -c $InterFiles/kmer_progeny_filter1.txt -s $Parent_stage_2/${i}_Stage2.txt" >> $InterFiles/DeleteMe.txt
+echo "python Getting_kmers_ready_for_R.py -i $Parent_stage_2/${file}.txt -l $Final_Kmers/usefull_kmers.txt -s $Parents_R_ready/${file}_Rready.txt" >> $InterFiles/EraseMe.txt
 done
-cat $InterFiles/DeleteMe.txt | parallel --jobs 3 --progress
-# use script one on second stage parent files
-python upper_lower_filter.py -k $Parent_stage_2 -u 1 -s $Final_Kmers/usefull_kmers.txt
- 
+cat $InterFiles/EraseMe.txt | parallel --jobs 4 --progress
 
-
+>$InterFiles/WipeMe.txt
+for file in $(ls $Progeny_KMERS | cut -d . -f 1)
+do
+echo "python Getting_kmers_ready_for_R.py -i $Progeny_KMERS/${file}.txt -l $Final_Kmers/usefull_kmers.txt -s $Progeny_R_ready/${file}_Rready.txt" >> $InterFiles/WipeMe.txt
+done
+cat $InterFiles/WipeMe.txt | parallel --jobs 4 --progress
 
 
 
